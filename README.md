4f328a049822544c79fc67446b58597bf58dfa8d
Shinosaki <git@shinosaki.com>
2023-06-24 17:20:14 +0900

    全体的に書き直し
        データをitestのjsonから取得してくるようにしたよ

--- README.md
+++ README.md
@@ -1,15 +1,26 @@
 # 2ch-api-worker
-2ch-api-workerは2chのHTMLをdat形式に変換するCloudflare Workersアプリケーションです。
+
+<!-- [![Deploy to Cloudflare Workers](https://deploy.workers.cloudflare.com/button)](https://deploy.workers.cloudflare.com/?url=https://github.com/shinosaki/2ch-api-worker) -->
+
+2ch-api-workerは2ちゃんねる(5ちゃんねる)のHTMLをdat形式に変換するCloudflare Workersアプリケーションです。
 
 ## ToDo
-- [ ] read.cgi ver 05系に対応
+- [] linkedomからHTMLRewriterに移行
+<!-- - [ ] read.cgi ver 05系に対応 -->
 - [ ] サーバー移転の追従  
-  例えば`https://5ch.net/poverty/1633076823/`にアクセスした場合`https://leia.5ch.net/test/read.cgi/poverty/1633076823/`ではなく`https://greta.5ch.net/test/read.cgi/poverty/1633076823/`にリダイレクトされて「datが存在しません。削除されたかURL間違ってますよ。」と返ってくる。
+  <!-- 例えば`https://5ch.net/poverty/1633076823/`にアクセスした場合`https://leia.5ch.net/test/read.cgi/poverty/1633076823/`ではなく`https://greta.5ch.net/test/read.cgi/poverty/1633076823/`にリダイレクトされて「datが存在しません。削除されたかURL間違ってますよ。」と返ってくる。 -->
 
 ## 使用方法
-2chのURL(`https://greta.5ch.net/test/read.cgi/poverty/1653843928/`)のドメイン名部分(`greta.5ch.net`)をWorkerのドメイン名(`2ch.lain.im`)に置き換えてアクセスするだけです。
+~~2chのURL(`https://greta.5ch.net/test/read.cgi/poverty/1653843928/`)のドメイン名部分(`greta.5ch.net`)をWorkerのドメイン名(`2ch.lain.im`)に置き換えてアクセスするだけです。~~  
 
-応答はShift-JISでエンコードされた[2chのdat](https://info.5ch.net/index.php/Monazilla/develop/dat)です。
+下記のいずれか(read.cgi or dat)のURL形式に対応してます。
+
+- `https://2ch.lain.im/test/read.cgi/<板/board>/<スレッドID/Thread ID>`
+- `https://2ch.lain.im/<板/board>/dat/<スレッドID/Thread ID>`
+
+応答はShift-JISでエンコードされた[2chのdat](https://info.5ch.net/index.php/Monazilla/develop/dat)です。  
+
+URL末尾にクエリパラメータ`?charset=utf-8`を追加するとUTF-8になります。
 
 ### chMateの設定
 [設定]→[実験的]→[検索に使うURL(追加用)]にコレを入力  
@@ -20,3 +31,15 @@
 ## 制限
 - 書き込みには対応してないよ！
 - 国外IPアドレスからのアクセスは[451 Unavailable For Legal Reasons](https://ja.wikipedia.org/wiki/HTTP_451)で弾かれるみたい
+
+## Deploy
+[Deploy Button](https://deploy.workers.cloudflare.com/?url=https://github.com/shinosaki/2ch-api-worker) or
+
+1. `git clone --recursive https://github.com/shinosaki/2ch-api-worker.git`
+2. `cd 2ch-api-worker`
+3. `npm i`
+4. `npx wrangler login`
+5. `npm run deploy`
+
+## License
+[MIT](https://raw.githubusercontent.com/shinosaki/2ch-api-worker/main/LICENSE)
--- src/index.js
+++ src/index.js
@@ -1,68 +1,73 @@
-import { parseHTML } from 'linkedom';
 import iconv from '../iconv-lite/lib/index.js';
-// import iconv from 'iconv-lite';
+
+const getServerName = async (board, tid) => {
+  const data = {
+    poverty: {
+      '0-': 'greta',
+    },
+  };
+
+  if (!data[board]) {
+    return await fetch(`https://itest.5ch.net/subbacks/${board}.json`)
+      .then(r=>r.json())
+      .then(j=>j.threads[0][2]);
+  }
+
+  const ranges = Object.keys(data[board]).map(v=>{
+    const [start, end] = v.split('-');
+    return { start, end };
+  });
+
+  const range = ranges.find(r=>r.start <= tid && (r.end || tid) >= tid);
+
+  return data[board][`${range.start}-${range.end}`];
+};
+
+const itestAPI = async (board, tid) => {
+
+  const subdomain = await getServerName(board, tid);
+  const url = `https://itest.5ch.net/public/newapi/client.php?subdomain=${subdomain}&board=${board}&dat=${tid}`;
+  const res = await fetch(url)
+  return res.json();
+};
+
+const apiToDat = (json) => {
+  return json.comments.map((c, i)=>{
+    const title = (i === 0) ? json.thread[5] : '';
+    const name  = c[1];
+    const email = c[2];
+    const date  = c[3];
+    const uid   = c[4];
+    const be    = c[5];
+    const body  = c[6];
+    return `${name}<>${email}<>${date} ID:${uid} BE:${be}<>${body}<>${title}`;
+  }).join('\n');
+}
 
 export default {
   async fetch(req, env, ctx) {
-    const res = await getThread(new URL(req.url));
 
-    const ab = await res.arrayBuffer();
-    const decoder = new TextDecoder('sjis');
-    const body = decoder.decode(ab);
+    const url = new URL(req.url);
+    const charset = url.searchParams.get('charset')?.toLowerCase();
 
-    if (body.includes('Good bye 011')) {
-      console.error(`Error: status code ${res.status}`)
-      return new Response('HTMLの取得に失敗しました\n使い方は https://github.com/shinosaki/2ch-api-worker/',{ status: 404 })
+    const path = url.pathname.split('/');
+    if (path[1] !== 'test' && path[2] !== 'dat') {
+      return new Response('Invalid request');
     }
 
-    const {document} = parseHTML(body);
-    const dat = parseThread(document);
+    const { board, tid } = (url.pathname.split('/')[1] === 'test')
+      ? url.pathname.match(/read\.cgi\/(?<board>\w+)\/(?<tid>\d+)/).groups
+      : url.pathname.match(/(?<board>\w+)\/dat\/(?<tid>\d+)/).groups;
 
-    const encoded = iconv.encode(dat, 'SJIS')
+    const json = await itestAPI(board, tid);
 
-    return new Response(encoded, {
+    const dat = (charset === 'utf-8')
+      ? apiToDat(json)
+      : iconv.encode(apiToDat(json), 'SJIS');
+
+
+    return new Response(dat, {
       headers: { 'content-type': 'text/plain; charset=shift_jis' }
-    })
+    });
   }
 }
-
-async function getThread(url) {
-  const path = url.pathname
-    .split(/[\/\.]/)
-    .filter(x => !x.match(/^\s*$|dat|test|read|cgi/));
-
-  const board = path[0];
-  const tid = path[1];
-
-  const ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/112.0';
-  const res = await fetch(`https://5ch.net/${board}/${tid}/`, {
-    headers: { 'user-agent': ua }
-  })
-
-  return res
-}
-
-function parseThread(document) {
-  let title = document.querySelector('.title').textContent.replace('\n', '');
-  const posts = document.querySelectorAll('.thread .post');
-  const result = Array.from(posts).map((post, index) => {
-    const get = selector => post.querySelector(selector);
-
-    title = (index === 0) 
-      ? title 
-      : '';
-    const name  = get('.name')?.textContent ?? '';
-    const email = get('.name a')?.href.replace('mailto:', '') ?? '';
-    const date  = get('.date')?.textContent ?? '';
-    const uid   = get('.uid')?.textContent ?? '';
-    let be = ''
-    if (get('.be')) {
-      const be = `BE:${get('.be a').href.replace('http://be.5ch.net/user/', '')}-${get('.be').textContent}`
-    }
-    const message = get('.message .escaped')?.innerHTML ?? '';
-
-    return `${name}<>${email}<>${date} ${uid} ${be}<>${message}<>${title}`;
-  }).join('\n');
-  return result;
-}
-
